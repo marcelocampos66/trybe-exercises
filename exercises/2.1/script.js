5c002cd4d925fe3ee2af291dd133729dbc623ee6
Marcelo Campos <31359152+marcelocampos66@users.noreply.github.com>
2020-11-16 18:35:13 -0300

    Update script.js

--- exercises/2.1/script.js
+++ exercises/2.1/script.js
@@ -1,8 +1,6 @@
 let student = "euMesmo";
 let studentSkills = 10;
-javascript-updates
-"Vou ter muito sucesso na programação!";
-=======
 //Eu sou Tryber e...
-master
+=======
+"Vou ter muito sucesso na programação!";
 console.log("Sucesso!");
