42981e34da32899836887c0737f744ec2d055534
Marcelo Campos <marcelo.campos.economista@gmail.com>
2020-11-26 13:23:48 -0300

    correcao do exercicio 10

--- modulo_1/bloco_4/dia_1/exe_10.js
+++ modulo_1/bloco_4/dia_1/exe_10.js
@@ -1,7 +1,7 @@
 let custo = 40;
 let valorVenda = 100;
 
-if (custo >= 0 && valorVenda >= 0) {
+if (custo >= 0 || valorVenda >= 0) {
     let impostoSobreOCusto = custo * 0.2;
     let valorCustoTotal = (custo + impostoSobreOCusto);
     let lucro = (valorVenda - valorCustoTotal);
