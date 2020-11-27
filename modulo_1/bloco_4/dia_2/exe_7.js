a6650af9e0d495e5a9d1e8a5bcda75c1291e1cd6
Marcelo Campos <marcelo.campos.economista@gmail.com>
2020-11-27 13:05:41 -0300

    Adiciona os exercicios bonus 1 e 2

--- modulo_1/bloco_4/dia_2/exe_7.js
+++ modulo_1/bloco_4/dia_2/exe_7.js
@@ -1,9 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let minorNumber = 0;
+let minorNumber = 999;
 
 for (index = 0; index < numbers.length; index += 1) {
-    numbers.sort(function(a,b){return a-b});
-    minorNumber = numbers[0];
+    if (numbers[index] < minorNumber) {
+        minorNumber = numbers[index];
+    };
 }
 
 console.log(minorNumber);
